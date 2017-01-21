14d73ca91023b23ac9fd8dc2e33344d48cab93da
OhmGeek <ryan@ohmgeek.co.uk>
2017-01-21 14:43:59 +0000

    Class based implementation

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
 $path_of_wkhtmltopdf = "/usr/bin/wkhtmltopdf";
 $read_filename = "http://google.co.uk";
-$output_filename = "test.pdf";
 
-$cmd = escapeshellcmd($path_of_wkhtmltopdf . " " . $read_filename . " " . $output_filename);
+$gen = new PDFGenerator();
+$gen->generate_pdf($read_filename,"test.pdf");
 
-shell_exec($cmd);
+
+class PDFGenerator {
+
+	public function __construct() {
+		$this->path_of_converter = "/usr/bin/wkhtmltopdf";
+	}
+
+	public function generate_pdf($input,$output_filename) {
+		$cmd = escapeshellcmd($this->path_of_converter . " " . $input . " " . $output_filename);
+		shell_exec($cmd);
+	}
+
+
+}
