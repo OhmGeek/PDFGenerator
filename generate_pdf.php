edc6f91726e5ef294d4a192eab5c51c03fa4508b
Ryan Collins <OhmGeek@users.noreply.github.com>
2017-02-10 15:50:31 +0000

    update for fix/post

--- generate_pdf.php
+++ generate_pdf.php
@@ -3,9 +3,11 @@ include_once 'vendor/autoload.php';
 
 use mikehaertl\wkhtmlto\Pdf;
 
-$html_data = $_GET['html'];
+$html_data = $_POST['html'];
 $options = array(
     'binary' => '/usr/bin/wkhtmltopdf',
+    'load-error-handling' => 'ignore',
+    'load-media-error-handling' => 'ignore',
 );
 
 $pdf = new Pdf($html_data);
